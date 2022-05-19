0ad26f9392f6f17a1db6df659fd8df1b6743d747
ex1one <vyvf11@gmail.com>
2022-05-20 01:13:35 +0500

    Selecting a cell for a move

--- src/components/Board/BoardComponent.tsx
+++ src/components/Board/BoardComponent.tsx
@@ -1,23 +1,30 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import CellComponent from '../Cell/CellComponent';
 import Board from '../../models/Board';
+import Cell from '../../models/Cell';
 
 interface IBoardComponentProps {
   board: Board;
   setBoard: (board: Board) => void;
 }
 
-const BoardComponent: FC <IBoardComponentProps> = ({ board, setBoard }) => (
-  <div className="board">
-    {board.cells.map((row) => (
-      row.map((cell) => (
-        <CellComponent
-          cell={cell}
-          key={cell.id}
-        />
-      ))
-    ))}
-  </div>
-);
+const BoardComponent: FC <IBoardComponentProps> = ({ board, setBoard }) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+  return (
+    <div className="board">
+      {board.cells.map((row) => (
+        row.map((cell) => (
+          <CellComponent
+            setSelectedCell={setSelectedCell}
+            cell={cell}
+            key={cell.id}
+            selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+          />
+        ))
+      ))}
+    </div>
+  );
+};
 
 export default BoardComponent;
--- src/components/Cell/CellComponent.tsx
+++ src/components/Cell/CellComponent.tsx
@@ -1,16 +1,22 @@
-import React, { FC } from 'react';
+import React, { FC, SetStateAction } from 'react';
 import Cell from '../../models/Cell';
 
 interface ICellComponentProps {
-  cell: Cell
+  cell: Cell;
+  selected: boolean;
+  setSelectedCell: React.Dispatch<SetStateAction<Cell | null>>
 }
 
-const CellComponent: FC <ICellComponentProps> = ({ cell }) => {
+const CellComponent: FC <ICellComponentProps> = ({ cell, selected, setSelectedCell }) => {
   console.log(cell);
   return (
-    <div className={['cell', cell.color].join(' ')}>
+    <button
+      type="submit"
+      onClick={() => setSelectedCell(cell)}
+      className={['cell', cell.color, selected ? 'selected' : ''].join(' ')}
+    >
       {cell.figure?.logo && <img src={cell.figure.logo} alt="" />}
-    </div>
+    </button>
   );
 };
 

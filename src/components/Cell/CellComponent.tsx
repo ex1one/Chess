02bf4d682c11d61bc0dacea6c9065f2db665962c
ex1one <vyvf11@gmail.com>
2022-05-19 19:07:16 +0500

    Made the logic of the Figure class

--- src/components/Cell/CellComponent.tsx
+++ src/components/Cell/CellComponent.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from 'react';
-import { Cell } from '../../models/Cell';
+import Cell from '../../models/Cell';
 
 interface ICellComponentProps {
   cell: Cell
 }
 
 const CellComponent: FC <ICellComponentProps> = ({ cell }) => (
-  <div className={['cell', cell.color].join(' ')} />
+  <div className={['cell', cell.color].join(' ')}>
+    {cell.figure}
+  </div>
 );
 
 export default CellComponent;
--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,5 +1,34 @@
-class Figure {
+import Colors from '../Colors';
+import Cell from '../Cell';
 
+export enum FigureNames {
+  FIGURE = 'Фигура',
+  KING = 'Король',
+  KNIGHT = 'Конь',
+  PAWN = 'Пешка',
+  QUEEN = 'Ферзь',
+  ROOK = 'Ладья',
+  BISHOP = 'Слон',
 }
 
+class Figure {
+  color: Colors;
+
+  logo: typeof logo;
+
+  cell: Cell;
+
+  name: FigureNames;
+
+  id: number;
+
+  constructor(color: Colors, cell: Cell) {
+    this.color = color;
+    this.cell = cell;
+    this.cell.figure = this;
+    this.logo = null;
+    this.name = FigureNames.FIGURE;
+    this.id = Math.random();
+  }
+}
 export default Figure;

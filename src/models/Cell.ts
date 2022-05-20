23e5501e849cf95203ceb37cfb8338b6ea61969f
ex1one <vyvf11@gmail.com>
2022-05-20 13:21:16 +0500

    Made a method of moving vertically horizontally

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -1,6 +1,7 @@
 import Colors from './Colors';
 import Figure from './figures/Figure';
 import Board from './Board';
+import figure from './figures/Figure';
 
 export default class Cell {
   readonly x: number;
@@ -46,17 +47,44 @@ export default class Cell {
   }
 
   isEmptyHorizontal(target: Cell): boolean {
+    if (this.y !== target.y) return false;
 
+    const min = Math.min(this.x, target.x);
+    const max = Math.max(this.x, target.x);
+
+    for (let x = min + 1; x < max; x += 1) {
+      if (!this.board.getCell(x, this.y).isEmpty()) {
+        return false;
+      }
+    }
+    return true;
   }
 
   isEmptyDiagonal(target: Cell): boolean {
+    const absX = Math.abs(target.x - this.x);
+    const absY = Math.abs(target.y - this.y);
+    if (absX !== absY) return false;
 
+    const dy = this.y < target.y ? 1 : -1;
+    const dx = this.x < target.x ? 1 : -1;
+
+    for (let i = 1; i < absY; i += 1) {
+      if (!this.board.getCell(this.x + dx * i, this.y + dy * i).isEmpty()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  setFigure(figure: Figure) {
+    this.figure = figure;
+    this.figure.cell = this;
   }
 
   moveFigure(target: Cell) {
     if (this.figure && this.figure.canMove(target)) {
       this.figure?.moveFigure(target);
-      target.figure = this.figure;
+      target.setFigure(this.figure);
       this.figure = null;
     }
   }
--- src/models/figures/Queen.ts
+++ src/models/figures/Queen.ts
@@ -13,7 +13,13 @@ class Queen extends Figure {
 
   canMove(target: Cell): boolean {
     if (!super.canMove(target)) return false;
-    return true;
+    if (this.cell.isEmptyVertical(target)) {
+      return true;
+    }
+    if (this.cell.isEmptyHorizontal(target)) {
+      return true;
+    }
+    return this.cell.isEmptyDiagonal(target);
   }
 }
 
